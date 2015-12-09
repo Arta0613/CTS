8b56699d965e72d13c66317ec73ad57af5f437ea
Mohammed Amin <arta0613@gmail.com>
2015-12-09 01:09:36 -0500

    Added model class to handle postgres id generator

---
+++ app/model/MyServerConfigStartup.java
@@ -0,0 +1,23 @@
+package model;
+
+import com.avaje.ebean.config.dbplatform.DbType;
+import com.avaje.ebean.config.dbplatform.IdType;
+import com.avaje.ebean.config.dbplatform.PostgresPlatform;
+import com.avaje.ebean.config.dbplatform.DbIdentity;
+import com.avaje.ebean.config.ServerConfig;
+import com.avaje.ebean.event.ServerConfigStartup;
+
+// This is needed to change the way Postgres handles
+// id generation
+
+public class MyServerConfigStartup implements ServerConfigStartup {
+    @Override
+    public void onStart(ServerConfig serverConfig) {
+        PostgresPlatform postgresPlatform = new PostgresPlatform();
+        DbIdentity dbIdentity = postgresPlatform.getDbIdentity();
+        dbIdentity.setSupportsGetGeneratedKeys(false);
+        dbIdentity.setSupportsSequence(true);
+        dbIdentity.setIdType(IdType.GENERATOR);
+        serverConfig.setDatabasePlatform(postgresPlatform);
+    }
+}
