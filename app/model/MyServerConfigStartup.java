790499d790897d4bc0ebffdf337a49fdb8bb329d
Mohammed Amin <arta0613@gmail.com>
2015-12-17 19:21:51 -0500

    yea

--- app/model/MyServerConfigStartup.java
+++ app/model/MyServerConfigStartup.java
@@ -1,22 +1,22 @@
-//package model;//package model;
-//
-//import com.avaje.ebean.config.ServerConfig;
-//import com.avaje.ebean.config.dbplatform.DbIdentity;
-//import com.avaje.ebean.config.dbplatform.IdType;
-//import com.avaje.ebean.config.dbplatform.PostgresPlatform;
-//import com.avaje.ebean.event.ServerConfigStartup;
-//
-///**
-// * Created by dima on 12/11/15.
-// */
-//public class MyServerConfigStartup implements ServerConfigStartup {
-//    @Override
-//    public void onStart(ServerConfig serverConfig) {
-//        PostgresPlatform postgresPlatform = new PostgresPlatform();
-//        DbIdentity dbIdentity = postgresPlatform.getDbIdentity();
-//        dbIdentity.setSupportsGetGeneratedKeys(false);
-//        dbIdentity.setSupportsSequence(true);
-//        dbIdentity.setIdType(IdType.GENERATOR);
-//        serverConfig.setDatabasePlatform(postgresPlatform);
-//    }
-//}
+package model;//package model;
+
+import com.avaje.ebean.config.ServerConfig;
+import com.avaje.ebean.config.dbplatform.DbIdentity;
+import com.avaje.ebean.config.dbplatform.IdType;
+import com.avaje.ebean.config.dbplatform.PostgresPlatform;
+import com.avaje.ebean.event.ServerConfigStartup;
+
+/**
+ * Created by dima on 12/11/15.
+ */
+public class MyServerConfigStartup implements ServerConfigStartup {
+    @Override
+    public void onStart(ServerConfig serverConfig) {
+        PostgresPlatform postgresPlatform = new PostgresPlatform();
+        DbIdentity dbIdentity = postgresPlatform.getDbIdentity();
+        dbIdentity.setSupportsGetGeneratedKeys(false);
+        dbIdentity.setSupportsSequence(true);
+        dbIdentity.setIdType(IdType.GENERATOR);
+        serverConfig.setDatabasePlatform(postgresPlatform);
+    }
+}
